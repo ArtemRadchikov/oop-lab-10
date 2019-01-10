c1745b2fabce1fa81a72ca51e90e4976fcf640bf
ArtemRadchikov <lis.lis.03.03@mail.ru>
2019-01-10 15:29:16 +0300

    final

---
+++ oop-lab-10/Class.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab_10
+{
+    class Student { }
+
+     abstract class Goods: IGoods,IOwner
+    {
+        private int price;
+        private string producingCountry;
+        private string name;
+
+        public int Price { get => price; set => price = value; }
+        public string ProducingCountry { get => producingCountry; set => producingCountry = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Goods(string name, string producingCountry,  int price)
+        {
+            this.Name = name;
+            this.ProducingCountry = producingCountry;
+            this.Price = price;
+        }
+
+        //реализация интерфейсов
+        public virtual float Discount()
+        {
+
+            //переопределил в технике
+            float resolt;
+            if(price>1000000)
+            {
+                resolt = (float)(0.85 * price);
+            }
+            else
+            {
+                resolt = (float)(0.9 * price);
+            }
+            return resolt;
+        }
+
+        public string Manufacturer { get { return producingCountry ; } }
+
+        abstract public void Print();
+
+        public override int GetHashCode()
+        {
+            return Price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (Price > obj.GetHashCode())
+            {
+                return true;
+            }
+            else
+            { 
+            return false;
+            }
+
+        }
+        public abstract override string ToString();
+    }
+
+    class Table : Goods
+    {
+        private string material;
+        public Table(string name, string producingCountry, int prise, string material) : base (name,producingCountry,prise)
+        {
+            this.Material = material;    
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Имя товара: {0}\tСтрона-производитель:{1}\tЦена:{2}\t", Name, ProducingCountry, Price);
+        }
+
+        public string Material { get => material; set => material = value; }
+
+        public override string ToString()
+        {
+            return "Type: " + GetType() + "Имя товара: " + Name + "\t Строна-производитель:" + ProducingCountry + "\t Цена: " + Price;
+        }
+    }
+   
+    sealed class Screan:Goods
+    {
+        private int height;
+        private int width;
+
+        public int Height { get => height; set => height = value; }
+        public int Width { get => width; set => width = value; }
+
+        public Screan(string name, string producingCountry, int prise, int height,int width) : base(name, producingCountry, prise)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Имя товара: {0}\tСтрона-производитель:{1}\tЦена:{2}\tРазрешение:{3}x{4}", Name, ProducingCountry, Price,Height,Width);
+        }
+
+        public override string ToString()
+        {
+            return "Type: " + GetType() + "Имя товара: " + Name + "\t Строна-производитель:" + ProducingCountry + "\t Цена: " + Price;
+        }
+    }
+
+    class Technique : Goods
+    {
+        private string operationPeriod;
+        private string owner;
+        public Technique(string name, string producingCountry, int prise, string operationPeriod) : base(name, producingCountry, prise)
+        {
+            this.OperationPeriod = operationPeriod;
+            //Console.WriteLine("Введите название компании-производителя:");
+            this.Owner = "1";
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Имя товара: {0}\tСтрона-производитель:{1}\tЦена:{2}\t", Name, ProducingCountry, Price);
+        }
+
+        //sealed - запрет переопределения метода(и свойсв_
+        //base- дайт доступ к членам базового класса
+        public override sealed float Discount()
+        {
+            float resolt;
+            if (base.Price > 1000000 && ProducingCountry!="China")
+            {
+                resolt = (float)(0.8 * base.Price);
+            }
+            else
+            {
+                resolt = (float)(0.9 * Price);
+            }
+            return resolt;
+        }
+        
+        //скрывает наследуемый член от Goods
+        public new string Manufacturer { get { return owner; } }
+
+        public string OperationPeriod { get => operationPeriod; set => operationPeriod = value; }
+        public string Owner { get => owner; set => owner = value; }
+    }  
+
+    class Monitor:Technique,IComparable<Monitor>,IComparer<Monitor>
+    {
+        private int diagonal;
+
+        public Monitor(string name, string producingCountry, int prise, string operationPeriod,int diagonal):base(name, producingCountry, prise, operationPeriod)
+        {
+            this.Diagonal = diagonal;
+        }
+
+        public int CompareTo(Monitor o)
+        {
+            Monitor p = o as Monitor;
+            if (p != null)
+                return this.Name.CompareTo(p.Name);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+
+        public int Compare(Monitor monitor1, Monitor monitor2)
+        {
+            if (monitor1.Diagonal > monitor2.Diagonal)
+            {
+                return 1;
+            }
+            else if (monitor1.Diagonal < monitor2.Diagonal)
+            {
+                return -1;
+            }
+            else return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            
+            if (this.Name == obj.ToString())
+                return true;
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        
+        public static bool operator ==(Monitor monitor1,Monitor monitor2)
+        {
+            if (monitor1.Name == monitor2.Name)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static bool operator !=(Monitor monitor1, Monitor monitor2)
+        {
+            if (monitor1.Name != monitor2.Name)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+
+        public int Diagonal { get => diagonal; set => diagonal = value; }
+    }
+
+    class Headphones:Technique
+    {
+        private ConsoleColor color;
+
+        public Headphones(string name, string producingCountry, int prise, string operationPeriod, ConsoleColor color) : base(name, producingCountry, prise, operationPeriod)
+        {
+            this.Color = color;
+        }
+
+        public new void Print()
+        {
+            Console.ForegroundColor = Color;
+            Console.WriteLine("Имя товара: {0}\tСтрона-производитель:{1}\tЦена:{2}\t", Name, ProducingCountry, Price);
+            Console.ForegroundColor =ConsoleColor.Gray;
+        }
+
+        public ConsoleColor Color { get => color; set => color = value; }
+    }
+
+    //Композиция
+    class PC : Technique
+    {
+        private Monitor monitorPC;
+        internal Headphones HeadphonesPS {get;set;}
+
+        public PC(string name, string producingCountry, int prise, string operationPeriod) : base(name, producingCountry, prise, operationPeriod)
+        {
+            this.MonitorPC=new Monitor("MonitorPC","Chine",0,"2года",20);
+            this.HeadphonesPS = new Headphones("HeadphonesPC", "Chine", 0, "2года", ConsoleColor.Red);
+        }
+
+        internal Monitor MonitorPC { get => monitorPC; set => monitorPC = value; }
+    }
+
+    //Агрегация
+    class Projector :Technique
+    {
+        private bool wirelessCommunication;
+        private Screan screan;
+
+        public Projector(string name, string producingCountry, int prise, string operationPeriod, bool wirelessCommunication,Screan screan) : base(name, producingCountry, prise,operationPeriod)
+        {
+            this.WirelessCommunication = wirelessCommunication;
+            this.Screan = screan;
+        }
+
+        public bool WirelessCommunication { get => wirelessCommunication; set => wirelessCommunication = value; }
+        internal Screan Screan { get => screan; set => screan = value; }
+    }
+
+    //class TestForSealed : Screan{}//не может быть производным от запечатанного типа 
+
+    abstract class Phone:Technique
+    {
+        public long Number { get; set; }
+        abstract public void Call();
+        public Phone(string name, string producingCountry, int prise, string operationPeriod,long number) : base(name, producingCountry, prise, operationPeriod)
+        {
+            this.Number = number;
+        }
+    }
+
+    class Smartphone : Phone, ICommonFunctionsForPhones, ICommonFunctionsForSmartphones
+    {
+        public Smartphone(string name, string producingCountry, int prise, string operationPeriod, long number,bool wifiConnection) : base(name, producingCountry, prise, operationPeriod, number)
+        {
+            this.WiFiConnection = wifiConnection;
+        }
+
+        public bool WiFiConnection { get; set; }
+
+        void ICommonFunctionsForPhones.Call()
+        {
+            Console.WriteLine("Звонки доступны по номеру{0}", base.Number);
+        }
+        public override void Call()
+        {
+            Console.WriteLine("Для получения подробной информации о функции Call, обратитесь по номеру *141*3*1#");
+        }
+
+        void ICommonFunctionsForSmartphones.Call()
+        {
+            //Console.WriteLine("Звонки доступны по номеру{0}", base.Number);
+
+            if (WiFiConnection)
+            {
+                Console.WriteLine("Возможны звонки через интернет");
+            }
+            else
+            {
+                Console.WriteLine("Интернет соединение отсутствует");
+            }
+        }
+    }
+
+    class Printer
+    {
+        public virtual void IAmPrinting(Goods goods)
+        {
+            Console.WriteLine(goods.GetType());
+            Console.WriteLine(goods.ToString());
+        }
+    }
+}
---
+++ oop-lab-10/Program.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+
+
+namespace oop_lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            Console.WriteLine();
+            Console.WriteLine("Task1");
+            Console.WriteLine();
+            Console.WriteLine("Создать необобщенную коллекцию ArrayList.");
+
+            var Task1 = new ArrayList();
+            Console.WriteLine("a. Заполните ее 5 - ю случайными целыми числами");
+            Random random = new Random();
+            //for(int i=1;i<=5;i++)
+            //{
+            //    Task1.Add(random.Next(0,200));
+            //}
+            Task1.AddRange(new int[] { random.Next(0, 10), random.Next(10, 20), random.Next(20, 30), random.Next(30, 40), random.Next(40, 50) });
+
+            printTask1();
+
+            Console.WriteLine("b.Добавьте к ней строку");
+            Task1.Add("b. String for Task1");
+            printTask1();
+
+
+
+            Console.WriteLine("c. Добавьте объект типа Student ");
+            Student student = new Student();
+            Task1.Add(student);
+            Task1.Reverse();
+            Task1.Add(new Student());
+            Task1.Reverse();
+
+            printTask1();
+
+            Console.WriteLine("d. Удалите заданный элемент");
+            Task1.RemoveAt(3);
+            Task1.Remove(student);
+
+            printTask1();
+
+            if (Task1.Contains("b. String for Task1"))
+            {
+                Console.WriteLine("Содержит");
+            }
+            else
+            {
+                Console.WriteLine("Не содержит");
+            }
+
+
+            void printTask1()
+            {
+                Console.WriteLine("Вывод Task1\nРазмер {0}", Task1.Count);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (object i in Task1)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ResetColor();
+            }
+            #endregion
+
+            #region Task2
+            Console.WriteLine();
+            Console.WriteLine("Task2");
+            Console.WriteLine();
+            Console.WriteLine("Создать обобщенную коллекцию в соответствии с вариантом задания и заполнить ее данными, тип которых определяется вариантом задания(колонка – первый тип).  ");
+
+            LinkedList<char> CharLinkedList = new LinkedList<char>();
+            Console.WriteLine("c.Добавьте другие элементы(используйте все возможные методы добавления для вашего типа коллекции).");
+            CharLinkedList.AddLast('B');
+            CharLinkedList.AddFirst('a');
+            CharLinkedList.AddLast('c');
+            for (int i = 0; i < 3; i++)
+            {
+                CharLinkedList.AddLast('c');
+                CharLinkedList.AddBefore(CharLinkedList.First, (char)('A' + i));
+                CharLinkedList.AddLast((char)('Z' + i));
+            }
+            CharLinkedList.AddBefore(CharLinkedList.Find('c'), 'C');
+            CharLinkedList.AddAfter(CharLinkedList.Find('B'), 'b');
+            CharLinkedList.AddAfter(CharLinkedList.Find('B'), 'b');
+
+
+            Console.WriteLine("a.Вывести коллекцию на консоль");
+            printCharLinkedList();
+
+            Console.WriteLine("\nb.Удалите из коллекции n последовательных элементов");
+
+            int number;
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number < CharLinkedList.Count)
+                {
+                    for (int i = 1; i <= number; i++)
+                    {
+                        //CharLinkedList.RemoveFirst();
+                        CharLinkedList.RemoveLast();
+                    }
+                }
+            }
+            printCharLinkedList();
+
+            Console.WriteLine(" d.Создайте вторую коллекцию(см.таблицу) и заполните ее данными из первой коллекции.\n e.Выведите вторую коллекцию на консоль.");
+
+            HashSet<char> CharHashSet = new HashSet<char>();
+            CharHashSet.UnionWith(CharLinkedList);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            foreach (char i in CharHashSet)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("f.Найдите во второй коллекции заданное значение.");
+            Console.WriteLine("Введите символ для поиска в CharLinkedList");
+
+            char symbol;
+            symbol = (char)Console.Read();
+            if (CharHashSet.Contains(symbol))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} содержит символ {1}", "CharLinkedList", symbol);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} не содержит символ {1}", "CharLinkedList", symbol);
+            }
+            Console.ResetColor();
+            void printCharLinkedList()
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (char i in CharLinkedList)
+                {
+                    Console.Write(i + "  ");
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            #endregion
+            #region Task3
+            Console.WriteLine("Task3");
+            LinkedList<Monitor> MonitorLinkedList = new LinkedList<Monitor>();
+
+            Monitor mon2 = new Monitor("mon2", "RB", 1500, "2", 1567);
+            Monitor mon4 = new Monitor("mon4", "RB", 2500, "2", 15);         
+
+            MonitorLinkedList.AddFirst(new Monitor("mon1", "RB", 5700, "2", 156));
+            MonitorLinkedList.AddFirst(new Monitor("mon3", "RB", 5020, "2", 15556));
+            MonitorLinkedList.AddFirst(mon4);
+            MonitorLinkedList.AddFirst(mon4);
+            MonitorLinkedList.AddLast(mon2);
+            MonitorLinkedList.AddLast(mon2);
+            MonitorLinkedList.AddLast(mon2);
+            MonitorLinkedList.AddLast(mon2);
+            
+            MonitorLinkedList.AddBefore(MonitorLinkedList.Find(mon2), mon4);
+            MonitorLinkedList.AddAfter(MonitorLinkedList.Find(mon4), mon2);
+            PrintMonitorLinkedList();
+
+            Console.WriteLine("\nУдалите из коллекции n последовательных элементов");
+
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                if (num < MonitorLinkedList.Count)
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+
+                        MonitorLinkedList.RemoveFirst();
+                    }
+                }
+            }
+            PrintMonitorLinkedList();
+
+            HashSet<Monitor> MonitorHashSet = new HashSet<Monitor>(MonitorLinkedList);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (Monitor i in MonitorHashSet)
+            {
+                Console.Write(i + "  ");
+            }
+            Console.WriteLine();
+            Console.ResetColor();
+
+
+            void PrintMonitorLinkedList()
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (Monitor i in MonitorLinkedList)
+                {
+                    Console.Write(i + "  ");
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+
+           
+            if (MonitorHashSet.Contains(mon4))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{0} содержит {1}", "MonitorLinkedList", mon4);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} не содержит {1}", "MonitorLinkedList", mon4);
+            }
+            Console.ResetColor();
+
+
+            #endregion
+            #region Task 4
+            #endregion
+            Console.WriteLine("Task4");
+            ObservableCollection<Monitor> MonitorObservableCollection = new ObservableCollection<Monitor>(MonitorHashSet);
+            MonitorObservableCollection.CollectionChanged += TryToChangeMonitorObservableCollection;
+            void print()
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (Monitor i in MonitorObservableCollection)
+                {
+                    Console.Write(i + "  ");
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+            }
+            MonitorObservableCollection.Add(mon2);
+            print();
+            MonitorObservableCollection.Remove(mon2);
+            print();
+        }
+
+        public static void TryToChangeMonitorObservableCollection(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Debug.Assert(false, "Вы изменили MonitorObservableCollection");
+        }
+
+    }
+}
